2350f4679f3ed6685ba76c085f9547903e0741c9
furqanahmedmtbc <45226315+furqanahmedmtbc@users.noreply.github.com>
2019-02-21 15:32:24 +0500

    Update empty.py

--- final_demo/empty.py
+++ final_demo/empty.py
@@ -1,1 +1,6 @@
-#hello world
+allison any status update
+allison who is my first patient for today
+allison who is my next patient
+allison how many appointment do i have for today
+allison when was last time patient was immunized 
+allison when was next well visit due for this patient
