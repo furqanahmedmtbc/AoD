2a18be7833d97937135a7306c9496f5fe0760329
furqanahmedmtbc <45226315+furqanahmedmtbc@users.noreply.github.com>
2019-02-23 09:38:55 +0500

    Update empty.py

--- final_demo/empty.py
+++ final_demo/empty.py
@@ -5,3 +5,14 @@ how many appointment do i have for today
 when was last time patient was immunized 
 when was next well visit due for this patient
 any status update
+
+
+import requests
+import simplejson
+
+data = {"file_name":"unable", "path":"test","text":"Sorry, I couldnt understand what you said."}
+
+data_json = simplejson.dumps(data)
+payload = {'json_payload': data_json}
+
+rs = requests.post("http://172.16.0.213:6020/text", json=data)
